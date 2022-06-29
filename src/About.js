2eed710c8e20461febfa49766047e548d49a3f45
Kostya Sterkhov <sterxovk@mail.ru>
2022-06-29 13:25:29 +0400

    GitPages

--- src/About.js
+++ src/About.js
@@ -3,7 +3,6 @@ import Jumbotron from './Components/Jumbotron';
 
 const About = () => (
   <>
-    <h1>Page About</h1>
     <Jumbotron />
   </>
 );
--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <Router>
         <NavBar />
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/users" element={<Users/>} />
           <Route path="/about" element={<About />} />
         </Routes>
